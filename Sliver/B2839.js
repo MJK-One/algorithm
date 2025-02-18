232c0ef95b13075053c9598082d4731655f991f0
김민준 <hanleejss012@naver.com>
2025-02-18 09:47:43 +0900

    Sliver 단계 2839 설탕배달달

--- Sliver/B2839.js
+++ Sliver/B2839.js
@@ -1,1 +1,20 @@
-Hello
+// 백준 2839번 실버4 설탕배달 문제
+let input = Number(require('fs').readFileSync('./input.txt'));
+
+let bag = 0;
+let sugar = input;
+
+while (true) {
+    if (sugar % 5 === 0) {
+        console.log(sugar / 5 + bag);
+        break;
+    }
+
+    if (0 > sugar ) {
+        console.log(-1);
+        break;
+    }
+
+    bag++;
+    sugar -= 3;
+}
