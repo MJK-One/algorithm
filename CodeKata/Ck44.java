a966cd7400e9ecb56f2639b17e81820bdb46d219
MJK-One <hanleejss012@naver.com>
2025-03-05 14:15:24 +0900

    코드카타 - 44번 최소직사각형형

---
+++ CodeKata/Ck44.java
@@ -0,0 +1,32 @@
+package CodeKata;
+
+public class Ck44 {
+    public int solution(int[][] sizes) {
+        int answer = 0;
+        int top_Weight = 0;
+        int top_Height = 0;
+
+        for(int i = 0; i < sizes.length; i++) {
+            int weight = sizes[i][0];
+            int height = sizes[i][1];
+
+            if(weight < height) {
+                int temp = weight;
+                weight = height;
+                height = temp;
+            }
+
+            if(top_Weight < weight) {
+                top_Weight = weight;
+            }
+            if(top_Height < height) {
+                top_Height = height;
+            }
+        }
+
+        answer = top_Weight * top_Height;
+
+        return answer;
+    }
+
+}
